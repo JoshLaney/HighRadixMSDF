270b317fb1bf511b8cc2bc9563b2a9ada812d04c
JoshLaney <jal3qa@virginia.edu>
2021-06-12 21:59:06 +0100

    more timing fun

--- PLL_Test/soc_system/synthesis/submodules/clock_div.v
+++ PLL_Test/soc_system/synthesis/submodules/clock_div.v
@@ -4,11 +4,11 @@ input clock, resetn;
 output clk_pos, clk_neg;
 
 (* preserve="true" *) reg clk_pos;
-wire clk_neg;
+(* preserve="true" *) wire clk_neg;
 
 assign clk_neg = ~clk_pos;
 always@(posedge clock)begin
-	if(~resetn) clk_pos <= 1'b0;
+	if(!resetn) clk_pos <= 1'b0;
 	else clk_pos<=~clk_pos;
 end
 
